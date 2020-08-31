27a3f99cb14bc10a607808b97ff829af1bca9ce5
siddharthdb <siddharthdb@gmail.com>
2020-08-30 22:27:28 -0400

    update procfile and port

--- app.py
+++ app.py
@@ -83,4 +83,5 @@ async def fxrates(request, fxdate=None):
         )
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, workers=4)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host="0.0.0.0", port=port, workers=4)
