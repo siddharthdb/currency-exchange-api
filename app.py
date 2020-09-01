765a02df88da5396fc90d139aa5a4e1c0a9dffb6
siddharthdb <siddharthdb@gmail.com>
2020-09-01 18:20:49 -0400

    Added base request args to change base currency

--- app.py
+++ app.py
@@ -6,50 +6,59 @@ from datetime import datetime
 from xml.etree import ElementTree
 from sanic import Sanic
 from sanic.response import json
-from datetime import datetime
 
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
-app = Sanic("FX Rates API")
+app = Sanic("Currency Exchange API")
 
-myclient = pymongo.MongoClient(os.environ.get('DB_URL'))
-mydb = myclient["currency-exchange"]
-mycol = mydb["fxrates"]
+# Base Currency is EUR from European Central Bank
+baseCurrency = "EUR"
+
+# Initialize the Mongo DB connection and get the document collection
+dbClient = pymongo.MongoClient(os.environ.get("DB_URL"))
+fxDB = dbClient["currency-exchange"]
+fxRates = fxDB["fxrates"]
 
 namespaces = {
     "gesmes": "http://www.gesmes.org/xml/2002-08-01",
     "eurofxref": "http://www.ecb.int/vocabulary/2002-08-01/eurofxref",
 }
 
+
 async def update_rates():
     print("Start FX Rate Updates...")
-    r = requests.get(os.environ.get('ECB_RATES_URL'))
+
+    r = requests.get(os.environ.get("ECB_RATES_URL"))
     envelope = ElementTree.fromstring(r.content)
 
-    result = mycol.find({},{ "_id": 0, "date": 1 })
+    # Get all the dates stored in the FX Rates Collection
+    result = fxRates.find({}, {"_id": 0, "date": 1})
     datecoll = []
     for r in result:
-        datecoll.append(r['date'])
+        datecoll.append(r["date"])
 
     data = envelope.findall("./eurofxref:Cube/eurofxref:Cube[@time]", namespaces)
     for d in data:
         date = d.attrib["time"]
+        # Insert record for the date not found in date list
         if not date in datecoll:
             rates = {
                 "date": d.attrib["time"],
                 "rates": {
                     c.attrib["currency"]: float(c.attrib["rate"]) for c in list(d)
-                }
+                },
             }
-            mycol.insert_one(rates)
+            fxRates.insert_one(rates)
+
     print("End FX Rate Updates...")
+
 
 @app.listener("before_server_start")
 async def initialize_scheduler(app, loop):
     try:
         scheduler = AsyncIOScheduler()
         scheduler.start()
-        scheduler.add_job(update_rates, "interval", minutes=30)
+        scheduler.add_job(update_rates, "interval", hours=12)
     except BlockingIOError:
         pass
 
@@ -58,9 +67,18 @@ async def initialize_scheduler(app, loop):
 async def index(request):
     return json({"message": "Welcome of FX Rate API."})
 
+
 @app.route("/latest", methods=["GET"])
 @app.route("/<fxdate>", methods=["GET"])
 async def fxrates(request, fxdate=None):
+    if "base" in request.args and request.args["base"][0] != "EUR":
+        baseCurrency = request.args["base"][0]
+    else:
+        # Base Currency is EUR from European Central Bank
+        baseCurrency = "EUR"
+
+    fx_rate = None
+
     if fxdate:
         try:
             dt = datetime.strptime(fxdate, "%Y-%m-%d")
@@ -73,16 +91,37 @@ async def fxrates(request, fxdate=None):
                 status=400,
             )
 
-        fx_rate = mycol.find_one({ "date": dt.strftime("%Y-%m-%d")}, { "_id": 0, "date": 1, "rates": 1 })
-        if fx_rate:
-            return json(fx_rate)
+        fx_rate = fxRates.find_one(
+            {"date": dt.strftime("%Y-%m-%d")}, {"_id": 0, "date": 1, "rates": 1}
+        )        
     else:
-        x = mycol.find_one()
+        fx_rate = fxRates.find_one({}, {"_id": 0, "date": 1, "rates": 1})
+        
+    if baseCurrency == "EUR":
+        return json({ "base": baseCurrency, "date": fx_rate["date"], "rates": fx_rate["rates"] })
+    
+    if fx_rate:                        
+        rates = fx_rate['rates']
+        
+        if baseCurrency in rates:
+            base_rate = round(rates[baseCurrency], 4)
+            rates = {
+                currency: round(rate / base_rate, 4) for currency, rate in rates.items()
+            }
+            rates["EUR"] = round(1 / base_rate, 4)
+
+            return json({"base": baseCurrency, "date": fx_rate["date"], "fxrates": rates})
+        else:
+            return json(
+                {"error": "Base '{}' is not supported.".format(base)}, status=400
+            )                
+    else:
         return json(
-            { "base": "EUR", "date": x["date"], "rates": x["rates"] }
+            {"error": "There is no data for date '{}'".format(dt)},
+            status=400,
         )
 
+
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
-    print(port)
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
