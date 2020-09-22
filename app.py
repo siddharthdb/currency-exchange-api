80dbe61f47025a15e962fafeb6dfb043c6decc46
Siddharth Bhawalkar <siddharthdb@gmail.com>
2020-09-22 16:31:24 -0400

    API changes to store date in ISO format

--- app.py
+++ app.py
@@ -28,6 +28,9 @@ namespaces = {
 
 
 async def update_rates():
+    """
+    # The Updates rates is a scheduled method which fetches the Currency data from https://www.ecb.europa.eu/stats/eurofxref/eurofxref-hist.xml
+    """
     print("Start FX Rate Updates...")
 
     r = requests.get(os.environ.get("ECB_RATES_URL"))
@@ -41,16 +44,16 @@ async def update_rates():
 
     data = envelope.findall("./eurofxref:Cube/eurofxref:Cube[@time]", namespaces)
     for d in data:
-        date = d.attrib["time"]
-        refreshDates.append(date)
+        date = datetime.strptime(d.attrib["time"], "%Y-%m-%d")
         # Insert record for the date not found in date list
         if not date in datecoll:
+            refreshDates.append(date)
             rates = {
-                "date": d.attrib["time"],
+                "date": datetime.strptime(d.attrib["time"], "%Y-%m-%d"),
                 "rates": {
                     c.attrib["currency"]: float(c.attrib["rate"]) for c in list(d)
-                },
-            }
+                }                
+            }            
             fxRates.insert_one(rates)
 
     print("End FX Rate Updates...")
@@ -61,7 +64,7 @@ async def initialize_scheduler(app, loop):
     try:
         scheduler = AsyncIOScheduler()
         scheduler.start()
-        scheduler.add_job(update_rates, "interval", minutes=20)
+        scheduler.add_job(update_rates, "interval", minutes=30)
     except BlockingIOError:
         pass
 
@@ -92,40 +95,79 @@ async def fxrates(request, fxdate=None):
                 {"error": "There is no data for dates older then 1999-01-04."},
                 status=400,
             )
-
+       
         fx_rate = fxRates.find_one(
-            {"date": dt.strftime("%Y-%m-%d")}, {"_id": 0, "date": 1, "rates": 1}
+            {"date": dt}, {"_id": 0, "date": 1, "rates": 1}
         )        
     else:
         latestRate = fxRates.find({}, {"_id": 0, "date": 1, "rates": 1}).sort('date', -1).limit(1)
         dumpJson = dumps(latestRate)
         fx_rate = loads(dumpJson[1:len(dumpJson) - 1])
 
-    if baseCurrency == "EUR":
-        rates = fx_rate["rates"]
-        rates["EUR"] = round(1, 4)
-        return json({ "base": baseCurrency, "date": fx_rate["date"], "rates": rates})
     
-    if fx_rate:                        
-        rates = fx_rate['rates']
-        
-        if baseCurrency in rates:
-            base_rate = round(rates[baseCurrency], 4)
-            rates = {
-                currency: round(rate / base_rate, 4) for currency, rate in rates.items()
-            }
-            rates["EUR"] = round(1 / base_rate, 4)
-
-            return json({"base": baseCurrency, "date": fx_rate["date"], "fxrates": rates})
+    if fx_rate:
+        if baseCurrency == "EUR":    
+            rates = fx_rate["rates"]
+            rates["EUR"] = round(1, 4)
+            return json({ "base": baseCurrency, "date": str(fx_rate["date"]), "rates": rates})
         else:
-            return json(
-                {"error": "Base '{}' is not supported.".format(base)}, status=400
-            )                
+            rates = fx_rate['rates']
+            
+            if baseCurrency in rates:
+                base_rate = round(rates[baseCurrency], 4)
+                rates = {
+                    currency: round(rate / base_rate, 4) for currency, rate in rates.items()
+                }
+                rates["EUR"] = round(1 / base_rate, 4)
+
+                return json({"base": baseCurrency, "date": str(fx_rate["date"]), "fxrates": rates})
+            else:
+                return json(
+                    {"error": "Base '{}' is not supported.".format(baseCurrency)}, status=400
+                )                
     else:
-        return json(
-            {"error": "There is no data for date '{}'".format(dt)},
-            status=400,
-        )
+        return json({"error": "There is no data for date '{}'".format(dt)},
+            status=400)
+
+@app.route("/history", methods=["GET"])
+async def fxrates(request):
+    if "base" in request.args and request.args["base"][0] != "EUR":
+        baseCurrency = request.args["base"][0]
+    else:
+        # Base Currency is EUR from European Central Bank
+        baseCurrency = "EUR"
+
+    if "start" in request.args and "end" in request.args:
+        start = datetime.strptime(request.args["start"][0], "%Y-%m-%d")
+        end = datetime.strptime(request.args["end"][0], "%Y-%m-%d")
+        
+        print(start, end)
+        dbresult = fxRates.find({ "date": end }, {"_id": 0, "date": 1, "rates": 1})
+        
+        dumpJson = dumps(dbresult)
+        fx_rate = loads(dumpJson[1:len(dumpJson) - 1])
+        if fx_rate:
+            if baseCurrency == "EUR":    
+                rates = fx_rate["rates"]
+                rates["EUR"] = round(1, 4)
+                return json({ "base": baseCurrency, "date": str(fx_rate["date"]), "rates": rates})
+            else:
+                rates = fx_rate['rates']
+                
+                if baseCurrency in rates:
+                    base_rate = round(rates[baseCurrency], 4)
+                    rates = {
+                        currency: round(rate / base_rate, 4) for currency, rate in rates.items()
+                    }
+                    rates["EUR"] = round(1 / base_rate, 4)
+
+                    return json({"base": baseCurrency, "date": str(fx_rate["date"]), "fxrates": rates})
+                else:
+                    return json(
+                        {"error": "Base '{}' is not supported.".format(baseCurrency)}, status=400
+                    )
+    else:
+        return json({"error": "Missing input args of start and end"}, status=400)
 
 @app.route("/refresh", methods=["GET"])
 async def refreshRates(request):
